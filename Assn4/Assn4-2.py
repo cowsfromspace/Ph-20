259b3dea40aacc2950e9c2798afd23517b968585
cowsfromspace <fersotj@yahoo.com>
2016-02-15 15:50:01 -0800

    Working on Assn4

--- Assn4/Assn4-2.py
+++ Assn4/Assn4-2.py
@@ -6,21 +6,23 @@ import sys
 # SETUP #
 #########
 n, h, v0, x0 = 100000, 0.0001, 0, 1 # Creating a default set of conditions
+problemNum = 0
 
 def setup():
-	global n, h, v0, x0
+	global n, h, v0, x0, problemNum
 	if len(sys.argv) == 6:
 		# Allowing command-line arguments
 		(n, h, v0, x0, problemNum) = (float(x) for x in sys.argv[1:])
-	else:
-		if len(sys.argv) == 2 and sys.argv[1].lower() == "help":
-			print "Command line arguments are allowed:\n \
-			Must be in form n h x0 v0\n \
-			Default values: \n \
-			n = 1000000\n \
-			h = 0.0001\n \
-			x0 = 0\n \
-			v0 = 1"
+		
+	elif len(sys.argv) == 2 and sys.argv[1].lower() == "help":
+		print "Command line arguments are allowed:\n \
+		Must be in form n h x0 v0 probNum\n \
+		Default values: \n \
+		n = 1000000\n \
+		h = 0.0001\n \
+		x0 = 0\n \
+		v0 = 1\n \
+		problemNum = 0 (does all)"
 setup()
 
 ################
@@ -35,7 +37,7 @@ velI = np.zeros(n)
 # Augment is the array multiplied for the implicit form
 # Explicit form lacks the factor of 1/(1 + h ** 2)
 
-augmentX = np.array([[1, h], [-h, 1]]) * 1#/(1 + h ** 2)
+augmentX = np.array([[1, h], [-h, 1]])
 augmentI = np.array([[1, h], [-h, 1]]) * 1 /(1 + h ** 2)
 motionX  = np.array([x0, v0])
 motionI  = np.array([x0, v0])
@@ -74,15 +76,14 @@ exactV = v0 * np.cos(t) - x0 * np.sin(t)
 ############
 def problem1():
 	'''Graphs phase space of data. Also used in problem 2'''
-	#plt.plot(velX,    posX,    'b')
-	#plt.plot(velI,    posI,    'r')
+	plt.plot(velX,    posX,    'b')
+	plt.plot(velI,    posI,    'r')
 	plt.plot(exactV,  exactX,  'g')
-	plt.plot((velS[:-1] + velS[1:])/2,    posS[1:], 'b')
-	plt.plot(velS,    posS, 'r')
+	plt.plot(velS,    posS,    'y') # With velS[:-1] + velS[1:])/2 and posS[:-1], this fits exact
 	plt.title("Vel & Pos: n = %d,  h = 0%s,  x0 = %.1f,  v0 = %.1f" % (n, str(h).strip('0'), x0, v0))
 	plt.ylabel("Position")
 	plt.xlabel("Vel")
-	plt.show()
+	plt.savefig('PhaseSpace-%d,0%s,%.1f,%.1f.png' % (n, str(h).strip('0'), x0, v0), bbox_inches='tight')
 
 def problem3():
 	'''Calculates E for all 4 methods'''
@@ -96,7 +97,7 @@ def problem3():
 	plt.plot(t, eApproxX, t, eApproxI)
 	plt.plot(t, eExact)
 	plt.plot(t, eApproxS)
-	plt.show()
+	plt.savefig("Energy-%d,0%s,%.1f,%.1f.png" % (n, str(h).strip('0'), x0, v0), bbox_inches='tight')
 
 def problem4():
 	k = (n - 100.0) / n
@@ -104,15 +105,24 @@ def problem4():
 	plt.xlabel("time")
 	plt.title("Symplectic Position mistakes:  n = %d,  h = 0%s,  x0 = %.1f,  v0 = %.1f" % (n, str(h).strip('0'), x0, v0))
 	plt.plot(t[k * n::], posS[k * n::], t[k * n::], exactX[k * n::])
-	plt.show()
+	plt.savefig("SymplecticPositionErrors-%d,0%s,%.1f,%.1f.png" % (n, str(h).strip('0'), x0, v0), bbox_inches='tight')
 
 	plt.ylabel("Velocity")
 	plt.xlabel("time")
 	plt.title("Symplectic Velocity mistakes: n = %d,  h = 0%s,  x0 = %.1f,  v0 = %.1f" % (n, str(h).strip('0'), x0, v0))
 	plt.plot(t[k * n::], posS[k * n::], t[k * n::], exactX[k * n::])
-	plt.show()
+	plt.savefig("SymplecticVelocityErrors-%d,0%s,%.1f,%.1f.png" % (n, str(h).strip('0'), x0, v0), bbox_inches='tight')
 
 
 if __name__ == "__main__":
-	
-	problem1()
+	if problemNum == 1:
+		problem1()
+	if problemNum == 3:
+		problem3()
+	if problemNum == 4:
+		problem4()
+	if problemNum == 0:
+		problem1()
+		problem3()
+		problem4()
+
